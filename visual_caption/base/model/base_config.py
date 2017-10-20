010fdd00cb3226a2fde44b910e49d1f2a608082f
ming616 <ming616@gmail.com>
2017-10-20 14:51:57 +0100

    add tf slim package

--- visual_caption/base/model/base_config.py
+++ visual_caption/base/model/base_config.py
@@ -14,51 +14,55 @@ max_grad_norm = 5
 max_max_epoch = 100
 
 # Learning rate for the initial phase of training.
-learning_initial_rate = 2.0
-learning_rate_decay_factor = 0.5
-learning_num_epochs_per_decay = 8.0
 
-dropout_keep_prob = 0.5
+dropout_keep_prob = 0.6
 initializer_scale = 0.08
 early_stopping = 100
 
 # base_dir = "/home/liuxiaoming/data/visual_caption/"
 base_dir = "/home/liuxiaoming/data/visual_caption/"
 
-
 class BaseConfig(object):
     def __init__(self, model_name, mode):
-        self.base_dir = base_dir
         self.model_name = model_name
         self.mode = mode  # train,test or validation
+
+        self.base_dir = base_dir # base dir for model
+        self.model_dir = os.path.join(self.base_dir, self.model_name) # for
+        self.module_dir = os.path.join(base_dir, self.model_name)
+
+        # number of GPUs
+        self.num_gpus = 1
+
+        # data type and drop_keep_ratre
+        self.data_type = tf.float32  # data type
         self.dropout_keep_prob = dropout_keep_prob
+        self.initializer_scale = initializer_scale
 
-        self.learning_initial_rate = learning_initial_rate
-        self.learning_rate_decay_factor = learning_rate_decay_factor
-        self.learning_num_epochs_per_decay = learning_num_epochs_per_decay
+        # leaning rate for training
+        self.learning_rate_min = 0.01,  # min learning rate.
 
+        self.learning_initial_rate = 0.2
+        self.learning_rate_decay_factor = 0.5
+        self.learning_decay_steps = 10000
+        self.learning_decay_rate = 0.96
+
+        self.early_stopping = early_stopping
+
+        # for
+        self.max_grad_norm = max_grad_norm
+        self.max_max_epoch = max_max_epoch  # max 。
         # If not None, clip gradients to this value.
         self.clip_gradients = 5.0
 
-        self.initializer_scale = initializer_scale
-        # self.batch_size = BATCH_SIZE
-        self.data_type = tf.float32
-
-        self.early_stopping = early_stopping
-        self.max_grad_norm = max_grad_norm
-        self.max_max_epoch = max_max_epoch  # max 。
-
-        self.module_dir = os.path.join(base_dir, self.model_name)
-
-        self.data_dir = os.path.join(self.module_dir, "data")
-        self.model_dir = os.path.join(self.module_dir, "model")
+        # for log and checkpoint
         self.log_dir = os.path.join(self.module_dir, "log")
         self.log_dir = os.path.join(self.log_dir, str(time.time()))
         self.checkpoint_dir = os.path.join(self.module_dir, "checkpoint")
 
         # for session and gpu
         config = tf.ConfigProto()
-        config.gpu_options.per_process_gpu_memory_fraction = 0.9  # 程序最多只能占用指定gpu50%的显存
+        config.gpu_options.per_process_gpu_memory_fraction = 0.9  # 程序最多只能占用指定gpu 90%的显存
         config.gpu_options.allow_growth = True  # 程序按需申请内存
         config.allow_soft_placement = True
         config.log_device_placement = False
--- visual_caption/base/model/base_model.py
+++ visual_caption/base/model/base_model.py
@@ -12,8 +12,8 @@ from abc import ABCMeta, abstractmethod
 from functools import wraps
 
 import tensorflow as tf
-
 from tensorflow.contrib.tensorboard.plugins import projector
+
 
 def timeit(f):
     def timed(*args, **kw):
@@ -89,6 +89,21 @@ class BaseModel(object):
 
         self._build_model()
 
+    def _next_device(self):
+        """Round robin the gpu device. (Reserve last gpu for expensive op)."""
+        if self._num_gpus == 0:
+            return ''
+        dev = '/gpu:%d' % self._cur_gpu
+        if self._num_gpus > 1:
+            self._cur_gpu = (self._cur_gpu + 1) % (self._num_gpus - 1)
+        return dev
+
+    def _get_gpu(self, gpu_id):
+        if self.config.num_gpus <= 0 or gpu_id >= self.config.num_gpus:
+            self.logger.error('error number of GPUs')
+            return ''
+        return '/gpu:%d' % gpu_id
+
     @timeit
     def _build_model(self):
         self._get_logger()
@@ -102,12 +117,10 @@ class BaseModel(object):
         self._build_summaries()
         self._build_fetches()
 
-
     @timeit
     @define_scope(scope_name='embeddings')
     def _build_embeddings(self):
         pass
-
 
     @timeit
     @define_scope(scope_name='inputs')
@@ -138,48 +151,43 @@ class BaseModel(object):
     @timeit
     @define_scope(scope_name='optimizer')
     def _build_optimizer(self):
-        self._optimizer = tf.train.AdamOptimizer()
+        # num_examples_per_epoch = self._data_reader.num_examples_per_epoch
+        # batch_size = self._data_reader.data_config.batch_size
+        # num_batches_per_epoch = (num_examples_per_epoch / batch_size)
+        # decay_steps = int(num_batches_per_epoch *
+        #                   self.config.learning_decay_steps)
+
+        self._learning_rate = tf.maximum(
+            self.config.learning_rate_min,  # min learning rate.
+            tf.train.exponential_decay(learning_rate=self.config.learning_initial_rate,
+                                       global_step=self._global_step,
+                                       decay_steps=self.config.learning_decay_steps,
+                                       decay_rate=self.config.learning_decay_rate))
+
+        # learning_rate = self._learning_rate
+        learning_rate = self.config.learning_initial_rate
+        tf.summary.scalar('learning_rate', learning_rate)
+        self._optimizer = tf.train.GradientDescentOptimizer(learning_rate)
 
     @timeit
     @define_scope(scope_name='train_op')
     def _build_train_op(self):
+        """
+        Set up the training ops.
+        """
+        trainables = tf.trainable_variables()
+        gradients = tf.gradients(self._loss, trainables)
 
-        num_examples_per_epoch = 10000
-        learning_initial_rate = tf.constant(self.config.learning_initial_rate)
-        batch_size = self._data_reader.data_config.batch_size
-        num_batches_per_epoch = (num_examples_per_epoch / batch_size)
-        decay_steps = int(num_batches_per_epoch *
-                          self.config.learning_num_epochs_per_decay)
+        with tf.device(self._get_gpu(self.config.num_gpus - 1)):
+            clipped_gradients, global_norm = tf.clip_by_global_norm(t_list=gradients,
+                                                                    clip_norm=self.config.max_grad_norm,
+                                                                    name='clip_by_global_norm')
+            tf.summary.scalar('global_norm', global_norm)
 
-        def _learning_rate_decay_fn(learning_initial_rate, global_step):
-            return tf.train.exponential_decay(
-                learning_initial_rate,
-                global_step,
-                decay_steps=decay_steps,
-                decay_rate=self.config.learning_rate_decay_factor,
-                staircase=True)
-
-        learning_rate_decay_fn = _learning_rate_decay_fn
-
-        with tf.name_scope("train_op") as scope_name:
-            trainables = tf.trainable_variables()
-            gradients = tf.gradients(self._cost, trainables)
-
-            # # take gradients of cost w.r.t all trainable variables
-            # clipped_gradients, _ = tf.clip_by_global_norm(gradients, self.config.max_grad_norm)
-
-            clipped_gradients = self.config.clip_gradients
-
-            # Set up the training ops.
-            self._train_op = tf.contrib.layers.optimize_loss(
-                loss=self._cost,
-                global_step=self._global_step,
-                learning_rate=self.config.learning_initial_rate,
-                optimizer=self._optimizer,
-                clip_gradients=clipped_gradients,
-                learning_rate_decay_fn=learning_rate_decay_fn
-            )
-            # tf.summary.scalar('train_op', self._train_op)
+        grads_and_vars = zip(clipped_gradients, trainables)
+        self._train_op = self._optimizer.apply_gradients(grads_and_vars=grads_and_vars,
+                                                         global_step=self._global_step,
+                                                         name='train_step')
 
     @timeit
     @define_scope(scope_name='summaries')
@@ -196,7 +204,6 @@ class BaseModel(object):
         meta_file = os.path.join(self._data_reader.data_config.embedding_dir, "metadata.tsv")
         embed.metadata_path = meta_file
         projector.visualize_embeddings(self._summary_writer, config)
-
 
     def _run_epoch(self, sess, num_epoch, mode):
         """
@@ -331,4 +338,5 @@ class BaseModel(object):
     @abstractmethod
     def _run_inference(self):
         raise NotImplementedError()
+
     pass
